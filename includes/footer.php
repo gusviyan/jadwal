b94adbc62d66d8a5a3e214363ea99dfd613891d4
vian-rsperpam <gusviyan@gmail.com>
2025-10-29 15:42:14 +0700

    Ignore Credentials Commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
  <div class="glass-footer">
-    <span>© IT Dept RS Permata Pamulang | All Right Reserved</span>
+    <span>© Gusviyan - IT Dept RS Permata Pamulang | All Right Reserved</span>
   </div>
